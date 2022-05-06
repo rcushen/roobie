f376562d7ac25d99b120209f815f5a89be836a9c
Ryan Cushen <cushenr@gmail.com>
2022-05-06 23:24:49 +1000

    Refactor results page components
    Results page components have been refactored to more clearly
    account for each results query state.

--- src/features/results/Results.js
+++ src/features/results/Results.js
@@ -11,79 +11,74 @@ import {
 
 const Results = () => {
     const resultsStatus = useSelector(selectResultsStatus);
-    const results = useSelector(selectResults);
-    const activeTags = useSelector(selectActiveTags);
-
-    if (resultsStatus === 'loading') {
-        return (
-            <div className="results-container">
-                <div className="content-container">
-                    <h1>Loading...</h1>
-                </div>
-            </div>
-        )
-    } else if (resultsStatus === 'succeeded') {
-        console.log(results.length)
-        if (results.length === 0) {
-            return (
-                <div className="results-container">
-                    <div className="content-container">
-                        <div className="results-header">
-                            <p>Here are your results!</p>
-                            <div className="results-tags-gallery">
-                                {
-                                    activeTags.map((tag, index) => {
-                                        return (
-                                            <span key={index} className="search-tag-selected">{tag}</span>
-                                        )
-                                    })
-                                }
-                            </div>
-                        </div>
-                        <div className="results-body">
-                            <p>No results :(</p>
-                        </div>
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div className="results-container">
-                    <div className="content-container">
-                        <div className="results-header">
-                            <p>Here are your results!</p>
-                            <div className="results-tags-gallery">
-                                {
-                                    activeTags.map((tag, index) => {
-                                        return (
-                                            <span key={index} className="search-tag-selected">{tag}</span>
-                                        )
-                                    })
-                                }
-                            </div>
-                        </div>
-                        <div className="results-body">
-                            {results.map((record, key) => {
-                                return <ResultCard key={key} details={record} />
-                            })}
-                        </div>
-                    </div>
-                </div>
-            )
-        };
-    } else if (resultsStatus === 'error') {
-        return (
-            <div className="results-container">
-                <div className="content-container">
-                    <h1>Oh no! Error...</h1>
-                </div>
-            </div>
-        )
+    switch (resultsStatus) {
+        case 'idle':
+            return <ResultsIdle />
+        case 'loading':
+            return <ResultsLoading />
+        case 'succeeded':
+            return <ResultsSuccess />
+        case 'error':
+            return <ResultsError />
     }
 };
 
-const ResultCard = (props) => {
-    const { details } = props;
+const ResultsIdle = () => {
+    return (
+        <div className="content-container">
+            <h1><a href='/'>Click here to search again!</a></h1>
+        </div>
+    )
+}
+
+const ResultsLoading = () => {
+    return (
+        <div className="content-container">
+            <h1>Loading...</h1>
+        </div>
+    )
+};
+
+const ResultsSuccess = () => {
+    const results = useSelector(selectResults);
+    const activeTags = useSelector(selectActiveTags);
+
+    return (
+        <div className="content-container">
+            <div className="results-header">
+                <p>Here are your results!</p>
+                <div className="results-tags-gallery">
+                    {
+                        activeTags.map((tag, key) => {
+                            return (
+                                <span key={key} className="search-tag-selected">{tag}</span>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className="results-body">
+                {
+                    results.map((record, key) => {
+                        return ( 
+                            <ResultCard key={key} record={record} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+};
+
+const ResultsError = () => {
+    return (
+        <div className="content-container">
+            <h1>Oh no! Error...</h1>
+        </div>
+    )
+};
+
+const ResultCard = ({ record }) => {
     const resultType = useSelector(selectResultType);
 
     // Helper functions
@@ -91,14 +86,14 @@ const ResultCard = (props) => {
 
     return (
         <div className="results-card">
-            <a href={details.website}><h1>{details.name}</h1></a>
+            <a href={record.website}><h1>{record.name}</h1></a>
             <div className="results-card-tags">
-                <a href="/">{details.category}</a>
-                <a href="/">{dollarSigns(details.price)}</a>
-                <a href="/">{details.location}</a>
-                {resultType == 'nearMe' ? <a href="/">{details.distance.toFixed(2)} km away</a> : ''}
+                <a href="/">{record.category}</a>
+                <a href="/">{dollarSigns(record.price)}</a>
+                <a href="/">{record.location}</a>
+                {resultType == 'nearMe' ? <a href="/">{record.distance.toFixed(2)} km away</a> : ''}
             </div>
-            <p>{details.description}</p>
+            <p>{record.description}</p>
         </div>
     )
 };
