68bc0af576d2eb5e673a9e00740af43da10ab4f9
Ryan Cushen <cushenr@gmail.com>
2022-06-12 18:28:41 +1000

    Add a 'no results' window

--- src/features/results/Results.js
+++ src/features/results/Results.js
@@ -83,7 +83,7 @@ const ResultsSuccess = () => {
             </div>
             <div className="results-body">
                 <div className="results-content">
-                    {
+                    { results.length === 0 ? <NoResults /> : 
                         results.map((record, key) => {
                             return ( 
                                 <ResultCard key={key} record={record} />
@@ -239,5 +239,26 @@ const ResultCard = ({ record }) => {
         </div>
     )
 };
+
+const NoResults = () => {
+    // Helper functions
+    const handleSearchAgainButtonClick = () => {
+        window.location.href = '/'
+    };
+
+    return (
+        <div className="no-results">
+            <h3>No results found</h3>
+            <p className="no-results-explanation">Unfortunately, we don't know anywhere that meets your search criteria. If you do, please <a href="mailto:hello@roobie.com.au">let us know!</a></p>
+            <button
+                className="search-again-button"
+                onClick={handleSearchAgainButtonClick}
+            >
+                Search Again
+            </button>
+            <p className="no-results-punchline">(in the meantime, try being less specific)</p>
+        </div>
+    )
+}
 
 export default Results;
